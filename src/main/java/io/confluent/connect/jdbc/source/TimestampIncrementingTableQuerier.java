fc7c37424ca01e2f4dc45c8bb02d0f25fa3ce731
Jiri Syrovy <jiri.syrovy@emnify.com>
2020-08-24 15:55:00 +0200

    Fixed database connection closed

--- src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
+++ src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
@@ -24,11 +24,15 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Collections;
 import java.util.TimeZone;
 import java.util.Optional;
