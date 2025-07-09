3d4f62b833639fa39bf74e57724318fcb46c929e
Wolfgang Schäfer <wolfgang.schaefer@emnify.com>
2025-07-09 16:16:13 +0200

    nxpg-4967: use all columns for ordering

--- src/test/java/io/confluent/connect/jdbc/source/TimestampIncrementingCriteriaTest.java
+++ src/test/java/io/confluent/connect/jdbc/source/TimestampIncrementingCriteriaTest.java
@@ -192,7 +192,7 @@ public class TimestampIncrementingCriteriaTest {
         + "GREATEST(COALESCE(\"myTable\".\"ts1\",0),"
         +  "COALESCE(\"myTable\".\"ts2\",0)) < ? "
         + "ORDER BY "
-        + "COALESCE(\"myTable\".\"ts1\",\"myTable\".\"ts2\") "
+        + "\"myTable\".\"ts1\",\"myTable\".\"ts2\" "
         + "ASC",
         builder.toString()
     );
@@ -208,7 +208,7 @@ public class TimestampIncrementingCriteriaTest {
         + "GREATEST(COALESCE(myTable.ts1,0),"
         + "COALESCE(myTable.ts2,0)) < ? "
         + "ORDER BY "
-        + "COALESCE(myTable.ts1,myTable.ts2) "
+        + "myTable.ts1,myTable.ts2 "
         + "ASC",
         builder.toString()
     );
